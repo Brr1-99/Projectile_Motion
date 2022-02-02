2c3c523fdb536d26df639d8e195b34f7cf97fe05
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-02-02 12:22:49 +0100

    Project finished

--- main.py
+++ main.py
@@ -1,27 +1,110 @@
 import pygame
 import sys
+import math
+from settings import *
 
 class Ball( object ):
-	def __init__(self, x, y, radius, color):
+	def __init__(self, screen, x , y, radius, color):
+		self.screen = screen
 		self.x = x
 		self.y = y
 		self.radius = radius
 		self.color = color
+	
+	def draw(self):
+		pygame.draw.circle(self.screen, self.color, (self.x,self.y) ,self.radius)
 
+	def draw_line(self, mouse_pos):
+		pygame.draw.line(self.screen, green, (self.x,self.y), mouse_pos, 2 )
+	
+	def draw_ball_path (self, pos_list):
+		if pos_list:
+			for position in pos_list:
+				pygame.draw.circle(self.screen, red, position, 2, width=2)
+	
+	def calculate_angle(self, mouse_pos):
+		try :
+			angle = math.atan((mouse_pos[1]-self.y)/-(mouse_pos[0]-self.x)) * 180 / math.pi
+			angle = 180 + angle if angle < 0 else angle
+		except:
+			angle = 90
+		return angle
+	
+	@staticmethod
+	def next_position(initX, initY, power, angle, time):
+		xVelocity = math.cos(angle*math.pi/180)*power
+		yVelocity = math.sin(angle*math.pi/180)*power
+	
+		xDistance = xVelocity * time
+		yDistance = (yVelocity * time) + ((-4.9*(time**2))/2)
+
+		next_x = round(xDistance + initX)
+		next_y = round(initY - yDistance )
+
+		return next_x, next_y
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Projectile Motion')
+
+ball = Ball(screen, initial_pos[0], initial_pos[1], radius , black)
+
+ball_path_list = []
+current_mouse_pos = None
+clock = pygame.time.Clock()
+angle = 0
+shoot = False
+power = 0
+time = 0
+
+def redraw():
+	screen.fill(white)
+	ball.draw()
+	if current_mouse_pos != None:
+			ball.draw_line(current_mouse_pos)
+	ball.draw_ball_path(ball_path_list)
+	pygame.display.update()
 
 if __name__ == '__main__':
-    pygame.init()
-    screen_width = 800
-    screen_height = 600
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption('Projectile')
-    clock = pygame.time.Clock()
+	pygame.init()
+	while True:
+		screen.fill(white)
+		clock.tick(fps)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+		if shoot:
+			if ball.y < height - radius :
+				time += 0.03
+				ball_path_list.append((ball.x, ball.y))
+				next_coordinates = ball.next_position( x, y, power, angle, time)
+				ball.x, ball.y = next_coordinates
+				if next_coordinates[0] < radius :
+					ball.x = radius + 1 + abs(next_coordinates[0])
+				elif next_coordinates[0] > width - radius:
+					ball.x = 2*width - next_coordinates[0] - radius + 1
+			else:
+				shoot = False
+				time = 0
+				ball.y = height - radius - 1
+				if ball.x < radius or ball.x > width - radius:
+					ball.x = initial_pos[0]
 
-        pygame.display.flip()
-        clock.tick(60)
+
+
+		redraw()
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == pygame.MOUSEMOTION:
+				current_mouse_pos = event.pos
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				if not shoot:
+					x,y = ball.x, ball.y
+					ball_path_list = []
+					mouse_position  = pygame.mouse.get_pos()
+					angle = ball.calculate_angle(mouse_position)
+					shoot = True
+					power = math.sqrt((mouse_position[1]-ball.y)**2 + (mouse_position[0]-ball.x)**2)/8
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_r:
+					ball.x , ball.y = initial_pos
---
+++ settings.py
@@ -0,0 +1,12 @@
+fps = 200
+width = 1280
+height = 720
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+radius = 15
+
+initial_pos = (width/2, height-radius -1)
